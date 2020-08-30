4be08e6177abf9da061196ead56376e40d157c7f
ALEX <35903333+AL3XPI@users.noreply.github.com>
2020-08-30 14:28:20 -0400

    mobile support

--- js/scalefix.js
+++
@@ -1,4 +0,0 @@
-var siteWidth = device-width;
-var scale = screen.width /siteWidth
-
-document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
